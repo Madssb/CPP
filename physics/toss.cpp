cd2772dfe72b85114d6df3c83b2f2536cec65b5a
mads <anderen72@gmail.com>
2022-04-11 21:26:36 +0200

    updated datatype from float to vec1d

--- physics/toss.cpp
+++ physics/toss.cpp
@@ -2,19 +2,24 @@
 
 using namespace std;
 
+struct vec1d{
+    float x;
+};
+
 int main(){
-    float x, v, dt; //declaring variables prior to assignment in cin
+    float dt; //declaring variables prior to assignment in cin
+    vec1d x, v;
     float t = 0.0; //must be initialized
     float g = -9.81; //gravitational field
     cout << "insert x0:" << endl;
-    cin >> x;
+    cin >> x.x;
     cout << "insert v0:" << endl;
-    cin >> v;
+    cin >> v.x;
     cout << "insert dt:" << endl;
     cin >> dt;
-    while (x > 0){
-        v = v + g*dt;
-        x = x + v*dt;
+    while (x.x > 0){ //Euler-cromer integration method!
+        v.x = v.x + g*dt;
+        x.x = x.x + v.x*dt;
         t += dt;
     }
     cout << "it took" << t <<"seconds for whatever to hit the ground" << endl;
