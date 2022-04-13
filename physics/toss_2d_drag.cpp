9a165ce4a524d7472b2c220088c255b91c73cb2f
mads <anderen72@gmail.com>
2022-04-13 13:31:18 +0200

    updated toss_2d_drag.cpp

--- physics/toss_2d_drag.cpp
+++ physics/toss_2d_drag.cpp
@@ -1,82 +1,13 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
-
-struct vec2d        //defining 2d vectors, and creating their "rules"
-{
-    float x, y;     //defines vec2d as a datatype
-    vec2d(float x_input = 0, float y_input = 0)
-    {
-        x = x_input;
-        y = y_input;
-    }
-    float norm(){
-        return sqrt(x*x+y*y);
-    }
-    vec2d unit(){
-        vec2d v(x,y);
-        return v/v.norm();
-    }
-    friend vec2d operator+(vec2d const &, vec2d const &);   //lets the + operator interact with vec2d. + is basically declared a friend and is let into vec2d's house.
-    friend vec2d operator-(vec2d const &, vec2d const &);   //lets - into struct house
-    friend vec2d operator*(vec2d const &, vec2d const &);   // lets * into struct house
-    friend vec2d operator/(vec2d const &, vec2d const &);
-
-    friend vec2d operator*(vec2d const &, float const &); //allows for multiplying a vector with a constant. requires 2 lines to be commutative
-    friend vec2d operator*(float const &, vec2d const &);   
-    friend vec2d operator/(vec2d const &, float const &); //allows for dividing a vector by a constant 
-    friend vec2d operator/(float const &, vec2d const &); //allows for dividing a constant by a vector (idk if i need this???)
-    friend ostream &operator<<(ostream &, const vec2d &); //allows for 
-
-};
-
-vec2d operator+(vec2d const &c1, vec2d const &c2)           // rules for addition of vectors
-{
-    return vec2d(c1.x + c2.x, c1.y + c2.y);
-}
-vec2d operator-(vec2d const &c1, vec2d const &c2)           //rules for subtracting vectors
-{
-    return vec2d(c1.x - c2.x, c1.y - c2.y);
-}
-vec2d operator*(vec2d const &c1, vec2d const &c2)           //rules for multiplying vectors (array behaviour)
-{
-    return vec2d(c1.x * c2.x, c1.y * c2.y);
-}
-
-vec2d operator/(vec2d const &c1, vec2d const &c2)           //rules for dividing vectors (array behaviour)
-{
-    return vec2d(c1.x / c2.x, c1.y / c2.y);
-}
-
-vec2d operator*(vec2d const &c1, float const &c2)           //rules for scaling vectors by multiplication
-{
-    return vec2d(c1.x * c2, c1.y * c2);
-}
-
-vec2d operator*(float const &c1, vec2d const &c2)           //rules for scaling vectors by multiplication, commutative property.
-{
-    return vec2d(c1 * c2.x, c1 * c2.y);
-}
-
-vec2d operator/(vec2d const &c1, float const &c2)           //rules for scaling vectors by dividing
-{
-    return vec2d(c1.x /c2, c1.y/c2);
-}
-ostream &operator<<(ostream &os, const vec2d &c2)
-{
-    os << "[" << c2.x << "," << c2.y <<"]";
-    return os;
-}
-
+#include "vec2d.h"
 //ignore stuff above, defines 2d vector behaviour!
-
 vec2d drag(vec2d v){
     float rho =1.2;     //density of air at sea level
     float A  = 0.1*M_PI;//cross section of ball with radius r=10cm
     float Cd = 0.47;    //Drag coefficient for a sphere
     return -0.5*rho*Cd*A*pow(v.norm(),2)*v.unit();
 }
-
 vec2d acceleration(vec2d v){
     vec2d g = {-9.81, 0}; //constant acceleration g acting on object [m/s**2]
     float m = 0.1;      //kg, mass of ball
@@ -84,7 +15,6 @@ vec2d acceleration(vec2d v){
     vec2d total_acceleration = drag_acceleration + g;
     return total_acceleration;
 }
-
 int main(){
 //initial conditions
     vec2d r = {5, 0}; //intial posistion r0 of object [m]
@@ -99,6 +29,6 @@ int main(){
         r = r + v*dt;
         n++;
     }
-    cout << "r_final:\t" << r << endl << "v_final:\t" << v << endl;
+    std::cout << "r_final:\t" << r << std::endl << "v_final:\t" << v << std::endl;
     return 0; 
 }
