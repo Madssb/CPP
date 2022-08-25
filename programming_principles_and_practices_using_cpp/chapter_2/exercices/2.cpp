69e5cd825bb9308625e36321511d3c4e9aa86136
mads <anderen72@gmail.com>
2022-08-25 09:44:21 +0200

    idek

---
+++ programming_principles_and_practices_using_cpp/chapter_2/exercices/2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Stand up\n";
+    std::cout << "rotate 90 degrees CCW\n";
+    std::cout << "take 2 steps.\n";
+    std::cout <<  "rotate 90 degrees CW\n";
+    std::cout << "take 4 steps\n";
+    std::cout << "rotate 90 degrees CW\n";
+    std::cout << "keep walking until collision with doorway\n";
+    std::cout << "pass through doorway\n";
+    std::cout << "walk until collision with wall\n";
+    std::cout << "rotate 90 degrees CW\n";
+    std::cout << "walk until doorswitch is to your right\n";
+    std::cout << "Rotate 90 degrees CCW.\n";
+    std::cout << "press switch\n";
+    std::cout << "walk until open door is to your left, exit through said door.\n";
+    std::cout << "Walk up stairs to your left.\n";
+    std::cout << "Cba continuing.\n";
+    return 0;
+}ø
+
